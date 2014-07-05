150af943be6a331e83873f2f08b89718ee1e8d05
Andreas Wolf <andreas.wolf@usta.de>
2014-07-05 19:10:07 +0200

    [FEATURE] Find voters by discriminators

--- Classes/AstaKit/FriWahl/Core/Domain/Repository/ElectionRepository.php
+++ Classes/AstaKit/FriWahl/Core/Domain/Repository/ElectionRepository.php
@@ -8,6 +8,7 @@ namespace AstaKit\FriWahl\Core\Domain\Repository;
 
 use AstaKit\FriWahl\Core\Domain\Model\Election;
 use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Persistence\QueryInterface;
 use TYPO3\Flow\Persistence\QueryResultInterface;
 use TYPO3\Flow\Persistence\Repository;
 
@@ -15,7 +16,7 @@ use TYPO3\Flow\Persistence\Repository;
  * Repository for elections and the voters in an election.
  *
  * The voters are part of this repository because they do not really form an aggregate root (according to the principles
- * of Domain Driven Design) – they are tied to their election and would exist without it.
+ * of Domain Driven Design) – they are tied to their election and wouldn't exist without it.
  *
  * @Flow\Scope("singleton")
  */
@@ -56,12 +57,13 @@ class ElectionRepository extends Repository {
 	/**
 	 * Finds voters by the given criteria.
 	 *
-	 * @param array $criteria
+	 * @param Election $election
+	 * @param array $criteria Array with criteria, with the fields to search as key-value pairs in the "fields" sub-array
 	 * @return QueryResultInterface
 	 *
 	 * @throws \Exception If no criteria are defined
 	 */
-	public function findVotersByCriteria($criteria) {
+	public function findVotersByCriteria(Election $election, $criteria) {
 		$queryParameters = $queryParts = array();
 		if (isset($criteria['fields']['givenName']) && $criteria['fields']['givenName'] != '') {
 			$queryParameters['givenName'] = $criteria['fields']['givenName'];
@@ -74,15 +76,45 @@ class ElectionRepository extends Repository {
 		if (count($queryParts) == 0) {
 			throw new \Exception('No criteria given for voter search', 1401630546);
 		}
+		$queryParameters['election'] = $election;
 
 		$dql = 'SELECT v FROM AstaKit\FriWahl\Core\Domain\Model\EligibleVoter v WHERE '
+			. ' v.election = :election AND '
 			. implode(' AND ', $queryParts);
+
+		// TODO implement support for discriminators
 
 		/** @var $query \Doctrine\ORM\Query */
 		$query = $this->entityManager->createQuery($dql);
 		$query->setParameters($queryParameters);
 
 		return $query->execute();
+	}
+
+	public function findVotersByDiscriminator(Election $election, $discriminator, $discriminatorValue) {
+		$dql = 'SELECT v
+			FROM AstaKit\FriWahl\Core\Domain\Model\VoterDiscriminator d
+			JOIN AstaKit\FriWahl\Core\Domain\Model\EligibleVoter v
+			WHERE d.voter = v AND d.identifier = :identifier AND d.value = :value AND v.election = :election';
+
+		/** @var $query \Doctrine\ORM\Query */
+		$query = $this->entityManager->createQuery($dql);
+
+		return $query->execute(array(
+			'election' => $election,
+			'identifier' => $discriminator,
+			'value' => $discriminatorValue
+		));
+	}
+
+	public function findOneVoterByDiscriminator(Election $election, $discriminator, $discriminatorValue) {
+		$result = $this->findVotersByDiscriminator($election, $discriminator, $discriminatorValue);
+
+		if (!$result) {
+			return null;
+		} else {
+			return $result[0];
+		}
 	}
 
 }
